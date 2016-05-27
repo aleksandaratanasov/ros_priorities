d864082d1649546436b19a91f0b09972667d41a5
aleksandaratanasov <redbaronqueen@gmail.com>
2016-05-27 17:51:49 +0200

    Moved common lib to main CMakeLists.txt - usage of ROS dependencies

--- common/include/mode.h
+++ common/include/mode.h
@@ -1,13 +1,42 @@
 #ifndef MODE_H
 #define MODE_H
+//#include <boost/concept_check.hpp>
+#include <boost/signals2.hpp>
+#include <boost/thread.hpp>
+#include <string>
 
+#define PRIORITY_LOWEST 0
+#define PRIORITY_HIGHEST 10
+
+// TODO Add timeout support like in CoB's Mode. The timeout is used to disposed of modes that have taken longer then specified
 class Mode
 {
+protected:
+  uint32_t priority;
+  bool finished;
+  std::string name;
 public:
-Mode();
-~Mode();
-Mode& operator=(const Mode& other);
-bool operator==(const Mode& other);
+  Mode();
+  Mode(uint32_t priority, std::string name);
+  ~Mode();
+  Mode& operator=(const Mode& other);
+  bool operator==(const Mode& other);
+  bool operator<(const Mode& other);
+  bool operator>(const Mode& other);
+  
+  void setPriority(uint32_t priority);
+  uint32_t getPriority();
+  
+  void setName(std::string name);
+  std::string getName();
+  
+  bool isFinished();
+  
+  void start();
+  void pause();
+  void stop();
+  
+  virtual void execute() = 0;
 };
 
 #endif // MODE_H
--- common/src/mode.cpp
+++ common/src/mode.cpp
@@ -1,8 +1,18 @@
 #include "../include/mode.h"
 
 Mode::Mode()
+  : priority(PRIORITY_LOWEST),
+    finished(true),
+    name("")
 {
 
+}
+
+Mode::Mode(uint32_t _priority, std::string _name)
+  : priority(_priority),
+    finished(true),
+    name(_name)
+{
 }
 
 Mode::~Mode()
@@ -12,10 +22,64 @@ Mode::~Mode()
 
 Mode& Mode::operator=(const Mode& other)
 {
-
+  name = other.name;
+  priority = other.priority;
+  finished = true;
 }
 
 bool Mode::operator==(const Mode& other)
 {
+  return this->priority == other.priority;
+}
+
+bool Mode::operator<(const Mode& other)
+{
+  return this->priority < other.priority;
+}
+
+bool Mode::operator>(const Mode& other)
+{
+  return this->priority > other.priority;
+}
+
+void Mode::setPriority(uint32_t _priority)
+{
+  priority = _priority;
+}
+
+uint32_t Mode::getPriority()
+{
+  return priority;
+}
+
+void Mode::setName(std::string _name)
+{
+  name = _name;
+}
+
+std::string Mode::getName()
+{
+  return name;
+}
+
+bool Mode::isFinished()
+{
+  return finished;
+}
+
+void Mode::start()
+{
 
 }
+
+void Mode::pause()
+{
+
+}
+
+void Mode::stop()
+{
+
+}
+
+
